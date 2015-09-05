980b9a154a8e9d9a2f382aee9aca561ba8449bea
Bjørn Einar <bjartnes@gmail.com>
2015-09-05 21:19:19 +0200

    added empty stream

--- MusikkSpiller/Program.fs
+++ MusikkSpiller/Program.fs
@@ -4,6 +4,20 @@ open System
 open System.Collections.Generic
 open System.IO
 open System.Linq
+type WaveStream() =
+    inherit Stream()
+    override this.CanRead with get () = true 
+    override this.CanSeek with get () = false 
+    override this.CanWrite with get () = false 
+    override this.Length with get () = failwith "no length I am infinite" 
+    override this.Position with get () = failwith "no position" 
+                           and set (value) = failwith "no set pos" 
+    override this.Flush() = ()
+    override this.Read(buffer: byte[], offset:int, count: int) = failwith "no read yet"
+    override this.Seek(offset:int64, origin: SeekOrigin):int64 = failwith "no seek"
+    override this.SetLength(value: int64) = failwith "no set length"
+    override this.Write(buffer: byte[], offset:int, count:int) = failwith "no write"
+
 //16383
 let PlayBeep (frequency:UInt16) (msDuration:int) (volume :UInt16 )= 
     let mStrm = new MemoryStream();
