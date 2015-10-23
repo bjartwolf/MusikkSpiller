980274fe4bf07612a9aebf2db68b02f1e75d4e93
Bjørn Einar <bjartnes@gmail.com>
2015-10-23 09:31:15 +0200

    fixed it

--- MusikkSpiller/Program.fs
+++ MusikkSpiller/Program.fs
@@ -5,15 +5,14 @@ open System.Linq
 
 let generateStream() =
     let frequency = 440us
-    let msDuration = 2000
-    let volume = 16300us    
+    let msDuration = 1000
+    let volume = 16300us
     let mStrm = new MemoryStream();
     let writer = new BinaryWriter(mStrm);
-    let TAU :double= 2.0 * Math.PI
     let formatChunkSize = 16
     let headerSize = 8
     let formatType= 1s
-    let tracks = 10s
+    let tracks = 1s
     let samplesPerSecond = 44100
     let bitsPerSample = 16s
     let frameSize = tracks * ((bitsPerSample + 7s) / 8s)
@@ -50,55 +49,37 @@ let strmAsSeq (stream: Stream): byte seq =
 let rec sound t = seq {
     // 'volume' is UInt16 with range 0 thru Uint16.MaxValue ( = 65 535)
     // we need 'amp' to have the range of 0 thru Int16.MaxValue ( = 32 767)
-    let volume = 16300us    
+    let volume = 16300us
     let samplesPerSecond = 44100
     let amp:double = (double)(volume >>> 2) // so we simply set amp = volume / 2
     let frequency = 440us
-    let TAU :double= 2.0 * Math.PI
-    let theta :double= (double)frequency * TAU / (double)samplesPerSecond;
-    let volume = 16300us    
+    let tau :double= 2.0 * Math.PI
+    let theta :double= (double)frequency * tau / (double)samplesPerSecond;
+    let volume = 16300us
     yield! BitConverter.GetBytes((uint16)(amp * Math.Sin(theta * (double)t)))
-    yield! sound ((t+1) % 100000)
+    if (t < 44100) then yield! sound ((t+1))
 } 
 
-type EnumeratorThingy() =
-    let mutable counter = 0
-    let getCurrent() = 
-        86uy + (BitConverter.GetBytes(counter) |> Seq.head)
-    interface IEnumerator<byte> with
-        member this.Current with get() = getCurrent()
-    interface System.Collections.IEnumerator with
-        member this.Current with get() = getCurrent() :> obj
-        member this.MoveNext() = counter <- counter + 1 
-                                 true 
-        member this.Reset() = ()
-    interface IDisposable with
-        member this.Dispose() = () 
-
-
-type Thingy(input: IEnumerable<byte> ) =
-    let enumthing = input.GetEnumerator()
-    interface IEnumerable<byte> with
-        member this.GetEnumerator() = enumthing
-    interface System.Collections.IEnumerable with
-        member this.GetEnumerator() = enumthing :> System.Collections.IEnumerator
-
+let takeSkip (s: IEnumerable<byte>) (n: int) : (byte[] * IEnumerable<byte>) = 
+    let c = Seq.cache s 
+    let skipValues = Math.Min(n,c |> Seq.length)
+    (c |> Seq.truncate n |> Seq.toArray , c |> Seq.skip skipValues)
 
 type WaveStream() =
    inherit Stream()
    let firstData = generateStream() |> strmAsSeq
-   let sounddata = (sound 0)  
-   let data = Seq.append firstData sounddata 
-   let mutable pos = 0
+   let sounddata = sound 0
+   let mutable data = Seq.append firstData sounddata 
    override this.CanRead with get () = true 
    override this.CanSeek with get () = false 
    override this.CanWrite with get () = false 
    override this.Read(buffer: byte[], offset:int, count: int) =
             let copyTo (dst:Array) (src: Array) = src.CopyTo(dst,offset)
-            let arr = data |> Seq.skip pos |> Seq.take count |> Seq.toArray
-            arr |> copyTo buffer 
-            pos <- pos + count
-            if (pos > 200000) then 0 else count
+            let (bytes,data') = takeSkip data count 
+            data <- data'
+            bytes |> copyTo buffer 
+            let length = bytes.Length
+            length
    override this.Seek(offset:int64, origin: SeekOrigin):int64 = failwith "no seek"
    override this.SetLength(value: int64) = failwith "no set length"
    override this.Write(buffer: byte[], offset:int, count:int) = failwith "no write"
@@ -110,9 +91,10 @@ type WaveStream() =
 [<EntryPoint>]
 let main argv = 
     let ws = new WaveStream()
-    let player = new System.Media.SoundPlayer(ws);
-    player
+    let ms = new MemoryStream()
+    ws.CopyTo(ms)
+    ms.Position <- 0L
+    let player = new System.Media.SoundPlayer(ms)
     player.Play()
-    printfn "%A" argv
     Console.ReadKey() |> ignore
-    0 // return an integer exit code
+    0 
