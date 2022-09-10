32ea36aeb0bf94424ab649fa579926bd491e3cb0
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-09-10 23:22:37 +0200

    A byte of some sort

---
+++ MusicPlayerTest/SolverTests.fs
@@ -0,0 +1,10 @@
+﻿module SolverTests
+open System
+open Xunit
+open MusicPlayerCore.Player
+
+[<Fact>]
+let ``Read a byte`` () =
+    let firstPart = guitarSol |> Seq.take 1 
+    Assert.Equal([| 235uy |], firstPart)
+
