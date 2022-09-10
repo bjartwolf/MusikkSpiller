5bb9c7082362693e7c9c211a22dead3558c24767
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-09-10 23:45:27 +0200

    Remove weatherstuffs

---
+++ MusicApi/Controllers/WaveController.fs
@@ -0,0 +1,18 @@
+﻿namespace MusicApi.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open MusicPlayerCore.Player 
+
+[<ApiController>]
+[<Route("[controller]")>]
+type WaveController (logger : ILogger<WaveController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() =
+        "foo"
