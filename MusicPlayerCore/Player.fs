a5e8faaba0224c662bd89d06fb92362229993c8b
bjorn-einar-bjartnes-4ss <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2022-09-10 22:48:25 +0200

    Pulling wave file format into wave module

--- MusicPlayerCore/Player.fs
+++ MusicPlayerCore/Player.fs
@@ -3,6 +3,7 @@
 module Player =
     open System
     open System.IO
+    open WaveFormat
 
     let samplesPerSecond = 44100
     let duration = 10*1000
@@ -53,32 +54,6 @@ module Player =
             |> Seq.map ( fun (t,x) -> (t, x.[2])) 
     //        |> Seq.map (snd >> fun x -> x.[2]) 
 
-    let getHeaders () =
-        let formatChunkSize = 16
-        let headerSize = 8
-        let formatType= 1s
-        let frameSize = tracks * ((bitsPerSample + 7s) / 8s)
-        let bytesPerSecond = samplesPerSecond * (int)frameSize
-        let waveSize = 4;
-        let samples = (int)(samplesPerSecond * msDuration / 1000)
-        let dataChunkSize = samples * (int)frameSize;
-        let fileSize = waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize;
-        seq {
-            yield [|'R'B ; 'I'B; 'F'B;'F'B |]
-            yield (BitConverter.GetBytes(fileSize))
-            yield [|'W'B ; 'A'B; 'V'B;'E'B |]
-            yield [|'f'B ; 'm'B; 't'B; ' 'B|] 
-            yield (BitConverter.GetBytes(formatChunkSize))
-            yield (BitConverter.GetBytes(formatType))
-            yield (BitConverter.GetBytes(tracks))
-            yield (BitConverter.GetBytes(samplesPerSecond))
-            yield (BitConverter.GetBytes(bytesPerSecond))
-            yield (BitConverter.GetBytes(frameSize))
-            yield (BitConverter.GetBytes(bitsPerSample))
-            yield [|'d'B ; 'a'B; 't'B;'a'B |]
-            yield BitConverter.GetBytes(dataChunkSize)
-         } |> Array.concat
-
     let amp (value: double) =
         let volume = 16300us
         let amp:double = (double)(volume >>> 2) 
@@ -94,7 +69,7 @@ module Player =
     type WaveStream() =
        inherit Stream()
        let sounddata = guitarSol 
-       let mutable data = Seq.append (getHeaders()) sounddata 
+       let mutable data = Seq.append (getHeaders tracks bitsPerSample samplesPerSecond msDuration) sounddata 
        override this.CanRead with get () = true 
        override this.CanSeek with get () = false 
        override this.CanWrite with get () = false 
---
+++ MusicPlayerCore/WaveHeaders.fs
@@ -0,0 +1,31 @@
+﻿module WaveFormat 
+    open System
+
+    let getHeaders tracks bitsPerSample samplesPerSecond msDuration =
+        let formatChunkSize = 16
+        let headerSize = 8
+        let formatType= 1s
+        let frameSize = tracks * ((bitsPerSample + 7s) / 8s)
+        let bytesPerSecond = samplesPerSecond * (int)frameSize
+        let waveSize = 4;
+        let samples = (int)(samplesPerSecond * msDuration / 1000)
+        let dataChunkSize = samples * (int)frameSize;
+        let fileSize = waveSize + headerSize + formatChunkSize + headerSize + dataChunkSize;
+        seq {
+            yield [|'R'B ; 'I'B; 'F'B;'F'B |]
+            yield (BitConverter.GetBytes(fileSize))
+            yield [|'W'B ; 'A'B; 'V'B;'E'B |]
+            yield [|'f'B ; 'm'B; 't'B; ' 'B|] 
+            yield (BitConverter.GetBytes(formatChunkSize))
+            yield (BitConverter.GetBytes(formatType))
+            yield (BitConverter.GetBytes(tracks))
+            yield (BitConverter.GetBytes(samplesPerSecond))
+            yield (BitConverter.GetBytes(bytesPerSecond))
+            yield (BitConverter.GetBytes(frameSize))
+            yield (BitConverter.GetBytes(bitsPerSample))
+            yield [|'d'B ; 'a'B; 't'B;'a'B |]
+            yield BitConverter.GetBytes(dataChunkSize)
+         } |> Array.concat
+
+
+
