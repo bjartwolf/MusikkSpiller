74a9d4046bead1e65948d828d3209afcb8e2b03c
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2025-02-09 15:58:24 +0100

    add back html page

--- SimulationApi/Program.fs
+++ SimulationApi/Program.fs
@@ -13,6 +13,32 @@ open Giraffe
 open Microsoft.IO
 open MusicPlayerCore
 
+module Views =
+    open Giraffe.ViewEngine
+
+    let layout (content: XmlNode list) =
+        html [] [
+            head [] [
+                title []  [ encodedText "SimulationApi" ]
+                link [ _rel  "stylesheet"
+                       _type "text/css"
+                       _href "/main.css" ]
+            ]
+            body [] content
+        ]
+
+    let partial () =
+        h1 [] [ encodedText "SimulationApi" ]
+
+    let index () =
+        [
+            partial()
+        ] |> layout
+
+let indexHandler () =
+    let view = Views.index ()
+    htmlView view
+
 // do we need to return time... time is annoying
 // maybe just ignore time and return the next values and the delta T
 // delta T can for now be implicit
@@ -20,7 +46,7 @@ open MusicPlayerCore
 // but we do yield the latest state vector implicitly in the URL
 type SimulationResult =
     {
-        value: double array 
+        values: double array 
         nextResult: Uri
     }
 let solver = Player.sol_time_invariant
@@ -30,20 +56,21 @@ let simulationHandler ((s1,s2,s3,s4,s5,s6): stateVector) =
     let nextValues = solver x |> Seq.take 100 |> Seq.toArray
     let nextState = nextValues |> Array.last
     let serializedState = sprintf "%f/%f/%f/%f/%f/%f" nextState.[0] nextState.[1] nextState.[2] nextState.[3] nextState.[4] nextState.[5] 
-    let model     = {  value = (nextValues |> Array.map (fun x -> x.[2])); nextResult= new Uri(sprintf "/simulation/%s" serializedState, UriKind.Relative)}
+    let model = {  values = (nextValues |> Array.map (fun x -> x.[2])); nextResult= new Uri(sprintf "/simulation/%s" serializedState, UriKind.Relative)}
     json model
     
 let initSimulation () =
     let nextValues = solver Player.x0 |> Seq.take 100 |> Seq.toArray
     let nextState = nextValues |> Array.last
     let serializedState = sprintf "%f/%f/%f/%f/%f/%f" nextState.[0] nextState.[1] nextState.[2] nextState.[3] nextState.[4] nextState.[5] 
-    let model = {  value = (nextValues |> Array.map (fun x -> x.[2])); nextResult= new Uri(sprintf "/simulation/%s" serializedState, UriKind.Relative)}
+    let model = { values = (nextValues |> Array.map (fun x -> x.[2])); nextResult= new Uri(sprintf "/simulation/%s" serializedState, UriKind.Relative)}
     json model
 
 let webApp =
     choose [
         GET >=>
             choose [
+                route "/" >=> indexHandler ()
                 route "/init/" >=> initSimulation () 
                 routef "/simulation/%f/%f/%f/%f/%f/%f" simulationHandler 
             ]
